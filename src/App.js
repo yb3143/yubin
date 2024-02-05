7ec62910c7549287702630612fc95ee454abb3f6
yubin <yb3143@gmail.com>
2024-02-05 15:51:57 +0900

    children

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Button from './Button';
 import Dice from './Dice'
 
 function App() {
     return (
         <div>
+            <div>
+                <Button>던지기</Button>
+                <Button>처음부터</Button>
+            </div>
             <Dice color="red" num={4}/>
         </div>
     );
