113954a1a425b423171b2adb3ba2a26e59c115f2
yubin <yb3143@gmail.com>
2024-02-05 16:46:23 +0900

    참조형 State

--- src/App.js
+++ src/App.js
@@ -8,14 +8,20 @@ function random(n) {
 
 function App() {
     const [num, setNum] = useState(1);
+    const [sum, setSum] = useState(0);
+    const [gameHistory, setGameHistory] = useState([]);
 
     const handleRollClick = () => {
         const nextNum = random(6);
         setNum(nextNum);
+        setSum(sum + nextNum); // => 주사위를 던질 때 마다 값을 더함
+        setGameHistory([...gameHistory, nextNum]); // => 나온 숫자 기록
     };
 
     const handleClearClick = () => {
         setNum(1);
+        setSum(0);
+        setGameHistory([]);
     }
 
     return (
@@ -24,7 +30,12 @@ function App() {
                 <Button onClick={handleRollClick}>던지기</Button>
                 <Button onClick={handleClearClick}>처음부터</Button>
             </div>
-            <Dice color="red" num={num}/>
+            <Dice color="blue" num={num}/>
+            <h2>total</h2>
+            <p>{sum}</p>
+            <h2>history</h2>
+            <p>{gameHistory.join(', ')}</p> 
+            {/* join 메소드: 아규먼트로 전달한 값을 배열의 각 요소들 사이사이에 넣어서 결과적으로 하나의 문자열로 만들어주는 메소드 */}
         </div>
     );
 }
