91684b24159ea2486fcf30a374a4f76a9455712c
yubin <yb3143@gmail.com>
2024-02-05 15:17:50 +0900

    입력하면 나오게 하는 react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Dice from './Dice'
+
+function App() {
+    return (
+        <div>
+            <Dice color="red" num={4}/>
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-  <div> hi </div>
-  </>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
