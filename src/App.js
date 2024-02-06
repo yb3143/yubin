adf15d09aaa7ed97890d36eb4eb2df60c9430b2b
yubin <yb3143@gmail.com>
2024-02-06 17:43:05 +0900

    css 적용하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import Board from './Board';
 import Button from './Button';
+import './App.css'
 
 function random(n) {
     return Math.ceil(Math.random() * n);
@@ -29,9 +30,11 @@ function App() {
     };
 
     return (
-        <div>
-            <Button color='blue' onClick={handleRollClick}>던지기</Button>
-            <Button color='red' onClick={handleClearClick}>처음부터</Button>
+        <div className='App'>
+            <div>
+                <Button className='App-button' color='blue' onClick={handleRollClick}>던지기</Button>
+                <Button className='App-button' color='red' onClick={handleClearClick}>처음부터</Button>
+            </div>
             <div>
                 <Board name="나" color="blue" gameHistory={myHistory}/>
                 <Board name="상대" color="red" gameHistory={otherHistory}/>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom';
 import App from './App'
+import './index.css'; // => head 태그 안에 style 태그 자동으로 작성
 
 ReactDOM.render(<App />, document.getElementById('root'));
