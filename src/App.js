fe5e91d2c0f3dd312e5654c7a1f3d0592137107e
yubin <yb3143@gmail.com>
2024-02-06 17:17:05 +0900

    인라인 스타일

--- src/App.js
+++ src/App.js
@@ -30,8 +30,8 @@ function App() {
 
     return (
         <div>
-            <Button onClick={handleRollClick}>던지기</Button>
-            <Button onClick={handleClearClick}>처음부터</Button>
+            <Button color='blue' onClick={handleRollClick}>던지기</Button>
+            <Button color='red' onClick={handleClearClick}>처음부터</Button>
             <div>
                 <Board name="나" color="blue" gameHistory={myHistory}/>
                 <Board name="상대" color="red" gameHistory={otherHistory}/>
